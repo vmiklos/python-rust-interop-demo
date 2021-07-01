d6db9e0066a2c64a89f23ebdcc05a0ef9282028d
Miklos Vajna <vmiklos@collabora.com>
2021-07-01 20:24:23 +0200

    add gettext example

---
+++ examples/gettext/src/main.rs
@@ -0,0 +1,28 @@
+thread_local! {
+    static TRANSLATIONS: std::cell::RefCell<Option<gettext::Catalog>> = std::cell::RefCell::new(None);
+}
+
+fn tr(english: &str) -> String {
+    TRANSLATIONS.with(|translations| {
+        let translations = translations.borrow();
+        match *translations {
+            Some(ref translations) => translations.gettext(english).to_string(),
+            None => english.to_string(),
+        }
+    })
+}
+
+fn set_language(language: &str) -> Result<(), Box<dyn std::error::Error>> {
+    let file = std::fs::File::open(format!("locale/{}/LC_MESSAGES/myapp.mo", language))?;
+    let catalog = gettext::Catalog::parse(file)?;
+    TRANSLATIONS.with(|translations| {
+        *translations.borrow_mut() = Some(catalog);
+    });
+    Ok(())
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let _ = set_language("hu");
+    println!("{}", tr("Name"));
+    Ok(())
+}
