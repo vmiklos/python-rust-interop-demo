61e8287f2f06e4f464f0de21351377c9bfcb230b
Miklos Vajna <vmiklos@collabora.com>
2021-08-13 21:06:06 +0200

    file: implement context manager, close and iterator

--- examples/file/src/lib.rs
+++ examples/file/src/lib.rs
@@ -1,9 +1,13 @@
+use pyo3::class::PyIterProtocol;
 use pyo3::prelude::*;
+use pyo3::types::PyBytes;
+use pyo3::types::PyType;
+use pyo3::PyObject;
+use std::io::BufRead;
 use std::io::Read;
+use std::ops::DerefMut;
 use std::sync::Arc;
 use std::sync::Mutex;
-use pyo3::types::PyBytes;
-use pyo3::PyObject;
 
 fn open_read(path: &str) -> anyhow::Result<Arc<Mutex<dyn Read + Send>>> {
     let ret: Arc<Mutex<dyn Read + Send>> = Arc::new(Mutex::new(std::fs::File::open(path)?));
@@ -11,8 +15,32 @@ fn open_read(path: &str) -> anyhow::Result<Arc<Mutex<dyn Read + Send>>> {
 }
 
 #[pyclass]
+struct PyReadIter {
+    inner: std::vec::IntoIter<String>,
+}
+
+#[pyproto]
+impl PyIterProtocol for PyReadIter {
+    fn __iter__(slf: PyRef<Self>) -> PyRef<Self> {
+        slf
+    }
+
+    fn __next__(mut slf: PyRefMut<Self>) -> Option<PyObject> {
+        let string: String;
+        match slf.inner.next() {
+            Some(value) => string = value,
+            None => {
+                return None;
+            }
+        };
+        let buf: Vec<u8> = string.into_bytes();
+        Python::with_gil(|py| Some(PyBytes::new(py, &buf).into()))
+    }
+}
+
+#[pyclass]
 struct PyRead {
-    read: Arc<Mutex<dyn Read + Send>>
+    read: Arc<Mutex<dyn Read + Send>>,
 }
 
 #[pymethods]
@@ -24,12 +52,54 @@ impl PyRead {
             Ok(PyBytes::new(py, &buf).into())
         })
     }
+
+    fn close(&mut self) -> PyResult<()> {
+        Ok(())
+    }
+
+    fn __enter__(&self) -> Self {
+        let read = self.read.clone();
+        PyRead { read }
+    }
+
+    fn __exit__(
+        &mut self,
+        _ty: Option<&PyType>,
+        _value: Option<&PyAny>,
+        _traceback: Option<&PyAny>,
+    ) -> bool {
+        true
+    }
+}
+
+#[pyproto]
+impl PyIterProtocol for PyRead {
+    fn __iter__(slf: PyRef<Self>) -> PyResult<Py<PyReadIter>> {
+        let mut guard = slf.read.lock().unwrap();
+        let mut reader = std::io::BufReader::new(guard.deref_mut());
+        let mut lines: Vec<String> = Vec::new();
+        loop {
+            let mut line = String::new();
+            if let Ok(len) = reader.read_line(&mut line) {
+                if len == 0 {
+                    break;
+                }
+                lines.push(line);
+                continue;
+            }
+            break;
+        }
+        let iter = PyReadIter {
+            inner: lines.clone().into_iter(),
+        };
+        Py::new(slf.py(), iter)
+    }
 }
 
 #[pyfunction]
 fn py_open_read(path: &str) -> PyResult<PyRead> {
     match open_read(path) {
-        Ok(value) => Ok(PyRead{read: value}),
+        Ok(value) => Ok(PyRead { read: value }),
         Err(err) => Err(pyo3::exceptions::PyOSError::new_err(format!(
             "open_read() failed: {}",
             err.to_string()
